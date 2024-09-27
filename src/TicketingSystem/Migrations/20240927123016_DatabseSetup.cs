efc0ca72bcadd0566d6a16eff0aaa78f63eac67a
Jan Drwiega <jan.drwiega@codeconcept.pl>
2024-09-27 14:31:45 +0200

    Added migrations for database

---
+++ src/TicketingSystem/Migrations/20240927123016_DatabseSetup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabseSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:TicketStatusEnum.ticket_status_enum", "open,in_progress,resolved")
+                .Annotation("Npgsql:Enum:TicketTypeEnum.ticket_type_enum", "bug,improvement,epic");
+
+            migrationBuilder.CreateTable(
+                name: "tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Assignee = table.Column<Guid>(type: "uuid", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    ReportedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ResolvedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    AffectedVersion = table.Column<string>(type: "text", nullable: true),
+                    RelatedElements = table.Column<Guid[]>(type: "uuid[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tickets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tickets");
+        }
+    }
+}
