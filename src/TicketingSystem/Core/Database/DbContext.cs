66173352dedc89986b244cc74f58f0aebe01e5ee
Jan Drwiega <jan.drwiega@codeconcept.pl>
2024-09-30 08:24:04 +0200

    Added manual logs

--- src/TicketingSystem/Core/Database/DbContext.cs
+++ src/TicketingSystem/Core/Database/DbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Core;
 using TicketingSystem.Common.Enums;
 using TicketingSystem.Common.Models;
 
@@ -8,13 +10,29 @@ namespace TicketingSystem.Core.Database
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<TicketEntity> TicketEntities { get; set; }
+        public Logger logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("logs/manual-logs-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TicketEntity>().ToTable("tickets");
-            modelBuilder.HasPostgresEnum<TicketTypeEnum>("TicketTypeEnum");
-            modelBuilder.HasPostgresEnum<TicketStatusEnum>("TicketStatusEnum");
-            base.OnModelCreating(modelBuilder);
+            try
+            {
+                modelBuilder.Entity<TicketEntity>().ToTable("tickets");
+                modelBuilder.HasPostgresEnum<TicketTypeEnum>("TicketTypeEnum");
+                modelBuilder.HasPostgresEnum<TicketStatusEnum>("TicketStatusEnum");
+                base.OnModelCreating(modelBuilder);
+
+                logger.Information("Database model created successfully");
+            }
+            catch (Exception exception)
+            {
+                logger.Fatal(exception, "Database model create failed");
+            }
+
         }
     }
 }
